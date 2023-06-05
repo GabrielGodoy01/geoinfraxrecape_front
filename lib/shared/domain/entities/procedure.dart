62184c981d8fb2f72966748c1cfcaddb6bf38ba9
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2023-06-05 14:49:23 -0300

    procedure entity

---
+++ lib/shared/domain/entities/procedure.dart
@@ -0,0 +1,17 @@
+class Procedure {
+  final String protcod;
+  final String nomePermissionariaCompleto;
+  final String nomePermissionaria;
+  final String lat;
+  final String long;
+  final DateTime dataTermReal;
+
+  Procedure({
+    required this.protcod,
+    required this.nomePermissionariaCompleto,
+    required this.nomePermissionaria,
+    required this.lat,
+    required this.long,
+    required this.dataTermReal,
+  });
+}
